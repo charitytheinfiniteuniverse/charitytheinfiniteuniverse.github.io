6dad53bf23814668cbc8176c41a1a4f2530df6d8
Charity The Infinite Universe <charitytheinfiniteuniverse@gmail.com>
2025-07-23 20:37:56 +0630

    Update darkmode.js

--- darkmode.js
+++ darkmode.js
@@ -1,10 +1,10 @@
 // darkmode.js
 function toggleDarkMode() {
   document.body.classList.toggle("dark-mode");
-  const icon = document.getElementById("modeToggle");
+  const btn = document.getElementById("modeToggle");
   if (document.body.classList.contains("dark-mode")) {
-    icon.textContent = "🌙";
+    btn.textContent = "🌙";
   } else {
-    icon.textContent = "☀️";
+    btn.textContent = "☀️";
   }
 }
