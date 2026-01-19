9510cdb1f6e4d88197fdf88b63d7a623f1dc89d3
Charity The Infinite Universe <charitytheinfiniteuniverse@gmail.com>
2026-01-19 13:22:49 +0630

    Create bookphotobutton.js

---
+++ bookphotobutton.js
@@ -0,0 +1,5 @@
+/* bookphotobutton.js အပိုင်း အစ */
+document.querySelector('.read-btn-style').addEventListener('click', function() {
+    console.log("Link သို့ သွားနေပါပြီ...");
+});
+/* bookphotobutton.js အပိုင်း အဆုံး */
