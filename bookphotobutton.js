42bda6de89b1f714eaa332b964305015caea08b0
Charity The Infinite Universe <charitytheinfiniteuniverse@gmail.com>
2026-01-19 19:44:49 +0630

    Delete bookphotobutton.js

--- bookphotobutton.js
+++
@@ -1,5 +0,0 @@
-/* bookphotobutton.js အပိုင်း အစ */
-document.querySelector('.read-btn-style').addEventListener('click', function() {
-    console.log("စာအုပ်ဖတ်ရန် သွားနေပါပြီ...");
-});
-/* bookphotobutton.js အပိုင်း အဆုံး */
